08c15e57e864c0a97207441653da7859e71f92e3
Kimberly <kimberly.323@hotmail.com>
2020-09-08 18:01:28 +0200

    Add project files.

---
+++ weekly1task5/weekly1task5.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "    1   2   3   4   5   6" << std::endl;
+	std::cout << "   --- --- --- --- --- ---" << std::endl;
+	std::cout << "A | + | + | + | + | + | + | A" << std::endl;
+	std::cout << "B | + | + | + | + | + | + | B" << std::endl;
+	std::cout << "C | + | + | + | + | + | + | C" << std::endl;
+	std::cout << "D | + | + | + | + | + | + | D" << std::endl;
+	std::cout << "   --- --- --- --- --- ---" << std::endl;
+	std::cout << "    1   2   3   4   5   6" << std::endl;
+
+}
